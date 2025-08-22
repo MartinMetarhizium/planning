63795a8b863005c2f72ca2d9541dceb92cc95114
unknown <martin.horn2014@gmail.com>
2025-08-22 14:15:41 -0300

    22-8 added pages to make it easy to follow

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ from io import BytesIO
 from constants import MIN_PROJECT_RATIO, DAILY_HOURS, PROJECT_MAP_BT, PROJECT_MAP, DEFAULT_END_DATE, DEFAULT_END_DATE_with_timezone, DEFAULT_START_DATE,DEFAULT_START_DATE_with_timezone
 from streamlit_javascript import st_javascript
 
+st.page_link("planning/pages/alan.py", label="Alan")
+st.page_link("planning/pages/martin.py", label="Martin")
 
-st.page_link("pages/martin.py", label="Martin")
-st.page_link("pages/alan.py", label="Alan")
